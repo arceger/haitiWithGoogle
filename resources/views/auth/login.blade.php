07afd9244d0035d88c9ded8d30d37c0ef3b655fc
Gerson Dos Santos Arce <arceger@br.ibm.com>
2021-05-16 16:08:52 -0300

    google2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,10 +68,9 @@
                                 <div class="form-group row">
                                    <div class="col-md-6 offset-md-4">
                                        <div class="form-check">
-                                       <div class="card-header">Ou Entre com sua Conta do Google</div>
-                                       <a href="{{ url('auth/google') }}" style="margin-top: 20px;" class="btn btn-sm btn-block">
-                                       <img src="btn_google_signin_dark_focus_web@2x.png" style="width: 189%;text-align: center;padding-right: 40px;">
-                                       </a>
+                                        <a href="{{ url('auth/google') }}" style="margin-top: 20px;" class="btn btn-sm btn-block">
+                                        <img src="btn_google_signin_dark_focus_web@2x.png" style="width: 189%;text-align: center;padding-right: 40px;">
+                                        </a>
                                        </div>
                                    </div>
                                 </div>
@@ -80,6 +79,10 @@
                         </div>
                     </form>
                 </div>
+
+                <div class="card-header">Ou Entre com sua Conta do Google</div>
+
+
             </div>
         </div>
     </div>
