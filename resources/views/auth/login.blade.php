3695c09b155f8179deeb967a02ef061f6b9dcb06
Gerson Dos Santos Arce <arceger@br.ibm.com>
2021-05-16 15:40:29 -0300

    google2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Laravel 7 Login</div>
+                <div class="card-header">Validar Acesso</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
